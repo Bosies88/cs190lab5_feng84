27a9bb8f4c472bb37edc87958012014173e68648
Bowen Feng <feng84@purdue.edu>
2013-11-15 15:41:25 -0500

    This is the lab template

---
+++ LabTemplate.java
@@ -0,0 +1,16 @@
+--2013-11-15 15:40:38--  https://github.com/scottopell/cs190lab5/raw/master/LabTemplate.java
+Resolving github.com... 192.30.252.131
+Connecting to github.com|192.30.252.131|:443... connected.
+HTTP request sent, awaiting response... 302 Found
+Location: https://raw.github.com/scottopell/cs190lab5/master/LabTemplate.java [following]
+--2013-11-15 15:40:39--  https://raw.github.com/scottopell/cs190lab5/master/LabTemplate.java
+Resolving raw.github.com... 199.27.73.133
+Connecting to raw.github.com|199.27.73.133|:443... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 1552 (1.5K) [text/plain]
+Saving to: 'LabTemplate.java.1'
+
+     0K .                                                     100% 11.3M=0s
+
+2013-11-15 15:40:39 (11.3 MB/s) - 'LabTemplate.java.1' saved [1552/1552]
+
